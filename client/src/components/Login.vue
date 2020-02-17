7da995f0a0dcbec55df9d0055f99e2be0261fd55
taeung <goodxodnd@naver.com>
2020-02-17 06:12:37 +0000

    Add Vuex (store.js) 02.17 -2

---
+++ client/src/components/Login.vue
@@ -0,0 +1,84 @@
+<template>
+<form class="form-Login">
+  <h1 class="h3 mb-3 font-weight-normal">Please Login</h1>
+  <label for="email" class="sr-only">Email address</label>
+  <input type="email" id="email" v-model="email" class="form-control" placeholder="Email address" required autofocus>
+  <label for="password" class="sr-only">Password</label>
+  <input type="password" id="password" v-model="password" class="form-control" placeholder="Password" required>
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Remember me
+    </label>
+  </div>
+  <button @click="login({ email, password})" class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+  <p class="mt-5 mb-3 text-muted"></p>
+</form>
+
+</template>
+
+
+<script>
+import { mapState, mapActions } from "vuex"
+import axios from 'axios';
+
+export default {
+    data() {
+        return{
+            email: null,
+            password: null
+            }
+    },
+    computed: {
+            ...mapState(["isLogin", "isLoginError"])
+    },
+    methods: {
+        ...mapActions(["login"]),
+
+            }
+}
+
+
+</script>
+
+<style scoped>
+
+body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-Login {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-Login .checkbox {
+  font-weight: 400;
+}
+.form-Login .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-Login .form-control:focus {
+  z-index: 2;
+}
+.form-Login input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-Login input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
---
+++ client/src/store.js
@@ -0,0 +1,110 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router'
+import axios from 'axios'
+import {isNull} from "bootstrap-vue/esm/utils/inspect";
+import {from} from "bootstrap-vue/esm/utils/array";
+
+// test it.
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{
+        userInfo: null,
+        isLogin: false,
+        isLoginError: true
+    },
+    mutations: {
+        //login success
+        loginSuccess(state){
+            state.isLogin = true,
+            state.isLoginError = false
+        },
+        //login fail
+        loginError(state){
+            state.isLogin = false,
+            state.isLoginError = true
+        },
+        logout(state) {
+            state.isLogin = false,
+            state.isLoginError = false,
+            sessionStorage.removeItem("access_token");
+
+        }
+    },
+    actions:{
+        getToken() {
+            const token = sessionStorage.getItem("access_token")
+            console.log(token)
+            return token
+        },
+        //try login
+        login({ commit}, loginObj) {
+            // Login => token return
+            axios
+                .post("/api/login", loginObj)
+                .then(res => {
+                    let response = res.data
+                    console.log(response)
+                    if(response['code'] == '200')
+                        {
+                        sessionStorage.setItem("access_token", response['access_token'])
+                        let token = sessionStorage.getItem("access_token")
+                        let config = {
+                            headers: {
+                                "Authorization": token
+                                }
+                            }
+                            commit('loginSuccess')
+                            router.push("/dashboard")
+                        }
+                    else {
+                        alert(' Check your Id&PW 1')
+                        }
+                })
+                .catch(() => {
+                           alert(' Check your Id&PW 2')
+                       })
+            },
+        register({ commit }, registerObj) {
+            if (registerObj['password'] == registerObj['passwordConfirm'])
+            {
+            axios
+                .post("/api/register", registerObj)
+                .then(res => {
+                    let response = res.data
+
+                    {
+                        console.log(response)
+                        if (response['code'] == '404') {
+                            alert(' Already exist ID')
+                        } else {
+                            alert(' Register Success')
+                            router.go(-1)
+                        }
+                    }
+                })
+            }
+            else {
+                alert('password is not same')
+            }
+
+
+        },
+        loginRefresh( {commit} ) {
+            let token = sessionStorage.getItem("access_token");
+            if (token != null) {
+                commit("loginSuccess")
+                router.push("/dashboard")
+            }
+            else
+                commit('loginError')
+
+        },
+        logout({commit}) {
+                commit("logout")
+                router.push("/login")
+            },
+    }
+})
+
