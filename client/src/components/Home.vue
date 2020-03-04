ba3416d8d39c4cffafaf0d758dc1758b2f1380f9
goodxodnd <60911258+goodxodnd@users.noreply.github.com>
2020-03-04 14:55:23 +0900

    Update Home.vue

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -3,28 +3,28 @@
 <div>
       <nav aria-label="breadcrumb">
 <ol class="breadcrumb" style="background-color: #f9bd5b">
-   <li class="breadcrumb-item"><a href="#">Home</a></li>
-   <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+	<li class="breadcrumb-item"><a href="#">Home</a></li>
+	<li class="breadcrumb-item active" aria-current="page">Dashboard</li>
 </ol>
 </nav>
 
 <!-- ETH content -->
 <div class="shadow-sm p-3 mb-4 bg-white rounded">
-   <p class="ETH content" style="" size="sm">
+	<p class="ETH content" style="" size="sm">
     <ethmodal />
-   </p>
+	</p>
 </div>
 
 <!-- View DApps content-->
 <div class="shadow-sm p-3 mb-4bg-white rounded">
-   <p class="h5" style="padding-bottom: 2px; padding-top: 0;">
-      <ion-icon name="list-box" class="red"></ion-icon>&nbsp; View DApps
-   </p>
+	<p class="h5" style="padding-bottom: 2px; padding-top: 0;">
+		<ion-icon name="list-box" class="red"></ion-icon>&nbsp; View DApps
+	</p>
    <hr color="grey" size="2px"  width="100%"/>
 
 <!-- Name, Public, Upload -->
-   <div>
-   <b-table-simple hover>
+	<div>
+	<b-table-simple hover>
     <b-thead head-variant="secondary">
       <b-tr>
         <b-th class= "Name"><strong>Name</strong></b-th>
@@ -38,7 +38,6 @@
         <b-td>{{item.name}}</b-td>
         <b-td>{{item.public}}</b-td>
         <b-td>{{date(item.upload_time.$date)}}</b-td>
-        <b-td><b-button @click="DeploymentDApps(item._id)">Deployment</b-button></b-td>
       </b-tr>
     </b-tbody>
   </b-table-simple>
@@ -47,80 +46,6 @@
 </div>
 </template>
 
-<script>
-import axios from 'axios'
-import moment from 'moment'
-
-export default {
-  name: 'first',
-  data() {
-    isShowing:false
-    return {
-        fields: [
-          'name',
-          'public',
-          'upload_time',
-          'deployment'
-        ],
-        items: []
-    }
-  },
-  methods:{
-  getJSONResponse () {
-    const token = sessionStorage.getItem("access_token")
-        //console.log(token)
-    const path = 'http://localhost:5000/show_dapp'
-    axios
-    .get(path, { params: {},
-        headers: {
-            "Authorization": token
-        }
-    })
-    .then(response => {
-       this.items = response.data.payload
-    })
-    .catch(error => {
-        // eslint-disable-next-line no-console
-        console.log(error)
-    })
-  },
-    DeploymentDApps(app_id){
-    const token = sessionStorage.getItem("access_token")
-    axios.post('/api/deploymentPost',
-        {dapp_id:app_id},
-        {headers: {
-            "Authorization": token
-           }
-        })
-    .then(response => {
-        let code
-        let err_name
-        let reason
-        code = response.data.code
-        err_name = response.data.err_name
-        reason = response.data.reason
-
-       if(code == '200'){
-        alert('Success')
-       }
-       if(code != '200'){
-        alert('Fail'+err_name+reason)
-       }
-    })
-    .catch(error => {
-        console.log(error)
-    })
-    },
-    date: function (date) {
-      return moment(date).format('YYYY/MM/DD h:mm a');
-    }
-  },
-    created() {
-      this.getJSONResponse()
-    }
-}
-
-</script>
 
 <style scoped>
 .a{
@@ -141,7 +66,7 @@ color: black;
 }
 
 .ul,ol {
-  padding-left: 10px;
+  padding-left: 10;
 }
 
 .Name{
@@ -156,7 +81,7 @@ color: black;
   font-size: 1.1em;
 }
 .UploadTime{
- width: 500px;
+ width:-500px;
   float: left;
   font-size: 1.1em;
 }
@@ -168,3 +93,5 @@ color: black;
  }
 
 </style>
+
+
