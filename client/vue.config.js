1261234089b2025fcda70f61ef00b9e591d85f56
taeung <goodxodnd@naver.com>
2020-03-09 10:35:15 +0000

    success render template 03.09

---
+++ client/vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  assetsDir: '../static',
+  baseUrl: '',
+  publicPath: undefined,
+  outputDir: path.resolve(__dirname, '../server/templates'),
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+};
